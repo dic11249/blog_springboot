f9a801b83bf31cba7844654d7303c8a044f56044
Bmo <dic11249@gmail.com>
2019-12-29 23:16:19 +0800

    修正首頁導向網址

--- src/main/java/com/bmo/web/IndexController.java
+++ src/main/java/com/bmo/web/IndexController.java
@@ -1,22 +1,36 @@
 package com.bmo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.bmo.service.PostService;
+import com.bmo.service.TypeService;
 
-import javassist.NotFoundException;
 
 /**
  * @author Bmo  2019-12-08  
  *
  */
 @Controller
-@RequestMapping("/admin")
 public class IndexController {
+	
+	@Autowired 
+	PostService postService;
+	
+	@Autowired 
+	TypeService typeService;
 
-	@GetMapping("/index")
-	public String index() {
-		return "admin/post";
+	@GetMapping("/")
+	public String index(@PageableDefault(size = 5, sort= {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+						Model model) {
+		
+		model.addAttribute("page", postService.listPost(pageable));
+		model.addAttribute("types", typeService.listTypeTop(6));
+		model.addAttribute("recommends", postService.listRecommendPostTop(6));
+		return "blog/index";
 	}
 }
